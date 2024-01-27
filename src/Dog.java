af2e5306b8565e4c9f64d1aca6dd098792ba6a6a
nyamarinette <nyamarinette2020@gmail.com>
2024-01-27 19:23:49 +0200

    first commit

---
+++ src/Dog.java
@@ -0,0 +1,14 @@
+public class Dog
+{
+   //private final boolean nName = false;
+
+    public Dog(String name, int age)
+    {
+        this.mName = name;
+        this.mAge = age;
+       System.out.println(this.mName + " - " + this.mAge);
+}
+private String mName;
+private int mAge;
+}
+
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Dog d = new Dog("kito", 1);
+        Dog d2 = new Dog("Le chien", 9);
+// A notre sortie nous avons les noms des chiens (Dog ) et leur Age.
+    }
+}
